8acbce25511f6dee6fd395084e958ea5047e94a4
Andreas Vinter-Hviid <andreas.vinterhviid@gmail.com>
2017-02-17 14:04:34 +0100

    Print ASTs as textual tree instead of svg.
    This makes it more handy to use from the command line, removes the dependency on diagrams, and is a lot easier. Data.Tree comes with a function for printing trees this way.

--- app/Main.hs
+++ app/Main.hs
@@ -4,13 +4,10 @@ import Lib
 import Scanner
 import Parser.Parser
 import Parser.Output
-import Diagrams.Prelude
-import Diagrams.Backend.SVG.CmdLine
-import Diagrams.Backend.SVG
 import System.Environment
 
 
 main :: IO ()
 main = do
     input <- getLine
-    mainWith $ (graphicalData (parseSpl input) :: Diagram B)
+    drawAst (parseSpl input)
