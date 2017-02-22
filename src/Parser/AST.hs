e9b20954c1656618a9f0edd2518a096284428322
Andreas Vinter-Hviid <andreas.vinterhviid@gmail.com>
2017-02-22 20:25:46 +0100

    Reformat AST definitions

--- src/Parser/AST.hs
+++ src/Parser/AST.hs
@@ -4,7 +4,15 @@ module Parser.AST where
 import Data.Data
 
 type Identifier = String
-data ASTType = BoolT | IntT | CharT | PairT ASTType ASTType | ListT ASTType
+
+data ASTType =
+      BoolT
+    | IntT
+    | CharT
+    | PairT ASTType ASTType
+    | ListT ASTType
     | PolyT Identifier deriving (Data, Show)
-data ASTReturnType = Void | ReturnType ASTType deriving (Data, Show)
+data ASTReturnType =
+      Void
+    | ReturnType ASTType deriving (Data, Show)
 data ASTFunType = FunType [ASTType] ASTReturnType deriving (Data, Show)
