59ed5bad80ea6a0a9ae71118a2d04384a10e390d
Andreas Vinter-Hviid <andreas.vinterhviid@gmail.com>
2017-02-22 20:26:25 +0100

    Add preliminary expression type to AST

--- src/Parser/AST.hs
+++ src/Parser/AST.hs
@@ -16,3 +16,19 @@ data ASTReturnType =
       Void
     | ReturnType ASTType deriving (Data, Show)
 data ASTFunType = FunType [ASTType] ASTReturnType deriving (Data, Show)
+
+data Op2 =
+      Plus | Minus | Times | Div | Mod
+    | Equal | Less | Greater | LessEq | GreaterEq | NotEq
+    | And | Or
+    | Cons
+--TODO: Add Fields, Op1, FunCall
+data ASTExp =
+      Var Identifier
+    | Op2E Op2 ASTExp ASTExp
+    | IntE Int
+    | CharE Char
+    | BoolE Bool
+    | NilE
+    | PairE ASTExp ASTExp
+    | ParanE ASTExp
