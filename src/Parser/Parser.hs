84fe96178b5377c1b9e12cd034c47dad46e0947f
Andreas Vinter-Hviid <andreas.vinterhviid@gmail.com>
2017-02-23 00:50:15 +0100

    Add operator precedence to exp parser.
    I used to approach of the slides of modifying the grammar, but I have written the code such that extra precedence level can easily be added (or removed).

--- src/Parser/Parser.hs
+++ src/Parser/Parser.hs
@@ -6,6 +6,9 @@ import Parser.AST
 
 import Control.Applicative
 
+tok :: (Eq a) => a -> b -> Parser a b
+tok a b = (eat a *> pure b)
+
 parse :: (Parser Token a) -> String -> a
 parse p s = (fst . head) (runParser (p <* eof) (alexScanTokens s))
 
@@ -27,33 +30,42 @@ returnTypep = (eat VoidTok *> pure Void)  <|> (ReturnType <$> typep)
 funTypep :: Parser Token ASTFunType
 funTypep = FunType <$> (many typep <* eat ArrowTok) <*> returnTypep
 
-tok :: (Eq a) => a -> b -> Parser a b
-tok a b = (eat a *> pure b)
+type Precedence = Int
 
-op2p :: Parser Token Op2
-op2p =
-        (tok PlusTok Plus)  <|> (tok MinusTok Minus) <|> (tok TimesTok Times)
-    <|> (tok DivTok Div)    <|> (tok ModTok Mod)
-    <|> (tok EqTok Equal)   <|> (tok NeTok NotEq)
-    <|> (tok LtTok Less)    <|> (tok LeTok LessEq)
-    <|> (tok GtTok Greater) <|> (tok GeTok GreaterEq)
-    <|> (tok AndTok And)    <|> (tok OrTok Or)
-    <|> (tok ColonTok Cons)
+op2p :: Precedence -> Parser Token Op2
+op2p 0 =  (tok EqTok Equal) <|> (tok NeTok NotEq)
+op2p 1 =
+        (tok LtTok Less)
+    <|> (tok LeTok LessEq)
+    <|> (tok GtTok Greater)
+    <|> (tok GeTok GreaterEq)
+op2p 2 = (tok ColonTok Cons)
+op2p 3 = (tok PlusTok Plus)  <|> (tok MinusTok Minus) <|> (tok OrTok Or)
+op2p 4 =
+        (tok TimesTok Times)
+    <|> (tok DivTok Div)
+    <|> (tok ModTok Mod)
+    <|> (tok AndTok And)
+
+highestPrecedence = 4 :: Int
 
 --TODO: Most forms of expressions
---TODO: Precedence of operators. For now everything associates to the right.
-expp :: Parser Token ASTExp
-expp = combine <$> expp' <*> expp'' where
+--TODO: Associativity. For now operators on the same precedence level always
+--      associate to the right
+expp :: Precedence -> Parser Token ASTExp
+expp pr = combine <$> expp' pr <*> expp'' pr where
     combine e1 Nothing = e1
     combine e1 (Just (o, e2)) = Op2E o e1 e2
 
     --TODO: What about Char?
-    expp' :: Parser Token ASTExp
-    expp' =
-            (tok FalseTok (BoolE False))
-        <|> (tok TrueTok (BoolE True))
-        <|> ((\(IntLitTok i) -> IntE i) <$> eat (IntLitTok 0))
-        <|> (eat LParTok *> expp <* eat RParTok)
+    expp' :: Precedence -> Parser Token ASTExp
+    expp' pr
+        | pr /= highestPrecedence = expp (pr + 1)
+        | otherwise =
+                (tok FalseTok (BoolE False))
+            <|> (tok TrueTok (BoolE True))
+            <|> ((\(IntLitTok i) -> IntE i) <$> eat (IntLitTok 0))
+            <|> (eat LParTok *> expp 0 <* eat RParTok)
 
-    expp'' :: Parser Token (Maybe (Op2, ASTExp))
-    expp'' = optional ((,) <$> op2p <*> expp)
+    expp'' :: Precedence -> Parser Token (Maybe (Op2, ASTExp))
+    expp'' pr = optional ((,) <$> op2p pr <*> expp pr)
