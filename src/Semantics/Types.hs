cfb866160ce47ddf5698d189669fb9484fa9cddd
Andreas Vinter-Hviid <andreas.vinterhviid@gmail.com>
2017-03-24 14:59:50 +0100

    Various work on types.
    Add more handy definitions for substitutions.
    Begin work of type inference for expressions.
    Use handy substitution definitions to avoid having to pass substitutions around and apply them everywhere manually.

--- src/Semantics/Types.hs
+++ src/Semantics/Types.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Semantics.Types where
     
 import Data.Maybe
 import Data.Data
 import Data.Set as Set
 
-type TVarId = String
+import Control.Monad.State
+
+import Parser.AST
+
+data TVarId = NamedTV String | InternalTV Int deriving (Data, Ord, Show, Eq)
 
 data Type =
       TBool
@@ -24,14 +29,51 @@ tmap f (TArrow ts t) = TArrow (fmap (tmap f) ts) (tmap f t)
 tmap f (TVar a) = f a
 tmap f t = t
 
+type Inference a = StateT Int Maybe a
+
+infer :: Inference a -> Maybe a
+infer a = evalStateT a 0
+
+freshVar :: Inference Type
+freshVar = do
+    freshVar <- get
+    put (freshVar + 1)
+    pure (TVar (InternalTV freshVar))
+
+
 data TypeScheme = TypeScheme [TVarId] Type
 
-data Substitution = Substitution [(TVarId, Type)] deriving (Show)
+newtype TypeContext = TypeContext [(Identifier,Type)] deriving (Monoid)
 
-subst :: Substitution -> Type -> Type
-subst (Substitution s) = tmap subst' where
-    subst' :: TVarId -> Type
-    subst' id = fromMaybe (TVar id) (lookup id s)
+--Should have a better Eq instance!!!
+data Substitution = Substitution [(TVarId, Type)] deriving (Show, Eq)
+
+class Substable a where
+    subst :: Substitution -> a -> a
+
+instance Substable Type where
+    subst (Substitution s) t = tmap subst' t where
+        subst' :: TVarId -> Type
+        subst' id = fromMaybe (TVar id) (lookup id s)
+
+instance Substable TypeContext where
+    subst s (TypeContext c) = TypeContext $ fmap (\(id,t) -> (id, subst s t)) c
+
+instance Substable TypeScheme where
+    subst s (TypeScheme a t) = TypeScheme a (subst s t)
+
+instance Substable Substitution where
+    subst s1 s2 = s1 <> s2
+
+instance (Substable a, Functor m) => Substable (StateT s m a) where
+    subst s = fmap (subst s)
+
+instance (Substable a) => Substable (Maybe a) where
+    subst s = fmap (subst s)
+
+instance (Substable a, Substable b) => Substable (a -> b) where
+    subst s f = \a -> subst s (f (subst s a))
+
 
 --When composing substitutions the resulting list will have overwritten
 --Substitutions at the end using this definition.
@@ -51,6 +93,8 @@ vars (TArrow ts t) = (unions $ fmap vars ts) `union` (vars t)
 vars (TVar a) = singleton a
 vars t = empty
 
+mgu' t1 t2 s = subst s mgu t1 t2
+
 mgu :: Type -> Type -> Maybe Substitution
 mgu (TVar id1) (TVar id2)
     | id1 == id2 = Just mempty
@@ -62,14 +106,12 @@ mgu t (TVar id)
     | notMember id (vars t) = Just (Substitution [(id, t)])
     | otherwise = Nothing
 mgu (TList t) (TList s) = mgu t s
-mgu (TPair t1 t2) (TPair u1 u2) = do
-    s  <- mgu t1 u1
-    s' <- mgu (subst s t2) (subst s u2)
-    pure (s <> s')
-mgu (TArrow ts t) (TArrow us u) = do
-    s <- mgu_aux mempty ts us
-    s' <- mgu (subst s t) (subst s u)
-    pure (s <> s') where
+mgu (TPair t1 t2) (TPair u1 u2) =
+    mgu t1 u1 >>=
+    mgu' t2 u2
+mgu (TArrow ts t) (TArrow us u) =
+    mgu_aux mempty ts us >>=
+    mgu' t u where
         mgu_aux s [] [] = Just s
         mgu_aux s (t1:t2) (u1:u2) = do
             s' <- mgu (subst s t1) (subst s u1)
@@ -77,3 +119,60 @@ mgu (TArrow ts t) (TArrow us u) = do
 mgu t1 t2
     | t1 == t2 = Just mempty
     | otherwise = Nothing
+
+opInType Plus = TInt
+opInType Minus = TInt
+opInType Times = TInt
+opInType Div = TInt
+opInType Mod = TInt
+opInType Equal = TInt
+opInType Less = TInt
+opInType Greater = TInt
+opInType LessEq = TInt
+opInType GreaterEq = TInt
+opInType NotEq = TInt
+opInType And = TBool
+opInType Or = TBool
+opInType Cons = undefined
+
+opOutType Plus = TInt
+opOutType Minus = TInt
+opOutType Times = TInt
+opOutType Div = TInt
+opOutType Mod = TInt
+opOutType Equal = TBool
+opOutType Less = TBool
+opOutType Greater = TBool
+opOutType LessEq = TBool
+opOutType GreaterEq = TBool
+opOutType NotEq = TBool
+opOutType And = TBool
+opOutType Or = TBool
+opOutType Cons = undefined
+
+typeInferExp' e ctx t s = subst s (typeInferExp e) ctx t
+
+typeInferExp :: ASTExp -> TypeContext -> Type -> Inference Substitution
+typeInferExp (IntE _) ctx t = lift $ mgu t TInt
+typeInferExp (BoolE _) ctx t = lift $ mgu t TBool
+typeInferExp (CharE _) ctx t = lift $ mgu t TChar
+typeInferExp NilE ctx t =
+    freshVar >>= \a ->
+    lift (mgu t (TList a))
+typeInferExp (Op2E Cons e1 e2) ctx t =
+    freshVar >>= \a ->
+    typeInferExp  e1 ctx a >>=
+    typeInferExp' e2 ctx (TList a) >>=
+    lift . (mgu' t (TList a))
+typeInferExp (Op2E o e1 e2) ctx t =
+    freshVar >>= \a ->
+    typeInferExp  e1 ctx (opInType o) >>=
+    typeInferExp' e2 ctx (opInType o) >>=
+    lift . (mgu' t (opOutType o))
+
+--Keep this for the presentation to show before vs after
+--typeInferExp (Op2E o e1 e2) ctx t = do
+--    s1 <- typeInferExp e1 ctx (opInType o)
+--    s2 <- typeInferExp e2 (subst s1 ctx) (opInType o)
+--    s3 <- lift $ mgu (subst (s1 <> s2) t) (opOutType o)
+--    pure (s1 <> s2 <> s3)
--- test/Spec.hs
+++ test/Spec.hs
@@ -11,9 +11,13 @@ import Test.Tasty.HUnit
 
 import Data.List
 import Data.Ord
+import Data.Maybe
 
 import Parser
+import Parser.AST
 import Parser.Output
+
+import Semantics.Types
 
 pppTestFile f = do
     prgrm1 <- readFile f
@@ -30,3 +34,36 @@ test_ppp_on_syntactically_correct_programs = do
     fileNames <- listDirectory dir
     let cases = fmap (dir </>) fileNames
     pure $ fmap (\s -> testCase s (pppTestFile s)) cases
+
+test_mgu = [
+        testCase "Pair successfull unification" test1,
+        testCase "Pair unsuccessfull unification" test2,
+        testCase "Arrow successfull unification" test3,
+        testCase "Arrow unsuccessfull unification" test4
+    ] where
+    test1 = subst u t1 @?= subst u t2  where
+        t1 = (TPair (TVar (NamedTV "a")) TBool)
+        t2 = (TPair TInt (TVar (NamedTV "b")))
+        u = fromJust $  mgu t1 t2
+    test2 = mgu t1 t2 @?= Nothing where
+        t1 = (TPair (TVar (NamedTV "a")) TBool)
+        t2 = (TPair TInt (TVar (NamedTV "a")))
+    test3 = subst u t1 @?= subst u t2  where
+        t1 = (TArrow [(TVar (NamedTV "a")), TBool] TVoid)
+        t2 = (TArrow [TInt, (TVar (NamedTV "b"))] TVoid)
+        u = fromJust $ mgu t1 t2
+    test4 = mgu t1 t2 @?= Nothing  where
+        t1 = (TArrow [(TVar (NamedTV "a")), TBool] TVoid)
+        t2 = (TArrow [TInt, (TVar (NamedTV "a"))] TVoid)
+
+
+test_type_inference =
+    [testCase "Int list cannot unify with bool" test1
+    ,testCase "Int list infers correctly" test2] where
+    test1 = (infer (typeInferExp e mempty a)) @?= Nothing where
+        e = Op2E Cons (IntE 1) NilE
+        a = TBool
+    test2 = subst s a @?= TList TInt where
+        s = fromJust (infer (typeInferExp e mempty a))
+        e = Op2E Cons (IntE 1) NilE
+        a = (TVar (NamedTV "a"))
